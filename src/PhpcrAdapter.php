da40c06497b8427881834a5e0d517e51cb81440f
David Buchmann <david.buchmann@liip.ch>
2015-01-27 16:13:40 +0100

    flysystem adapter for phpcr

---
+++ src/PhpcrAdapter.php
@@ -0,0 +1,451 @@
+<?php
+
+namespace League\Flysystem\Phpcr;
+
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Config;
+use League\Flysystem\Util;
+use PHPCR\NodeInterface;
+use PHPCR\PathNotFoundException;
+use PHPCR\PropertyType;
+use PHPCR\SessionInterface;
+use PHPCR\Util\NodeHelper;
+use PHPCR\Util\PathHelper;
+
+/**
+ * Flysystem adapter for the PHP content repository.
+ *
+ * @author David Buchmann <mail@davidbu.ch>
+ */
+class PhpcrAdapter extends AbstractAdapter
+{
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * Constructor.
+     *
+     * @param SessionInterface $session
+     * @param string           $root
+     */
+    public function __construct(SessionInterface $session, $root)
+    {
+        $this->session = $session;
+        if (!$session->nodeExists($root)) {
+            $parent = NodeHelper::createPath($this->session, PathHelper::getParentPath($root));
+            $parent->addNode(PathHelper::getNodeName($root), 'nt:folder');
+        } else {
+            $rootNode = $this->session->getNode($root);
+            if (!$rootNode->isNodeType('nt:folder')) {
+                throw new \LogicException(sprintf(
+                    'The root node at %s must be of type nt:folder, found %s',
+                    $root ,
+                    $rootNode->getPrimaryNodeType()->getName())
+                );
+            }
+        }
+        $this->setPathPrefix($root);
+    }
+
+    /**
+     * Set the path prefix.
+     *
+     * @param string $prefix
+     *
+     * @return self
+     */
+    public function setPathPrefix($prefix)
+    {
+        parent::setPathPrefix($prefix);
+        if (null === $this->pathPrefix) {
+            $this->pathPrefix = $this->pathSeparator;
+        }
+    }
+
+    /**
+     * Prefix a path.
+     *
+     * @param string $path
+     *
+     * @return string prefixed path
+     */
+    public function applyPathPrefix($path)
+    {
+        $path = ltrim($path, '/');
+        if (strlen($path) === 0) {
+            return rtrim($this->getPathPrefix(), '/');
+        }
+
+        return $this->getPathPrefix() . $path;
+    }
+
+    /**
+     * Ensure the directory exists and try to create it if it does not exist.
+     *
+     * @param string  $path path relative to the $root.
+     * @param boolean $create Whether to create the folder if not existing.
+     *
+     * @return NodeInterface The node at $root/$path.
+     */
+    private function getFolderNode($path, $create = false)
+    {
+        $location = $this->applyPathPrefix($path);
+        if (!$create && !$this->session->nodeExists($location)) {
+            // trigger the phpcr exception rather than trying to create parent nodes.
+            return $this->session->getNode($location);
+        }
+
+        $folders = array();
+        while (!$this->session->nodeExists($location)) {
+            $folders[] = PathHelper::getNodeName($location);
+            $location = PathHelper::getParentPath($location);
+        }
+
+        $node = $this->session->getNode($location);
+        while (null !== $folder = array_pop($folders)) {
+            $node = $node->addNode($folder, 'nt:folder');
+        }
+        if (!$node->isNodeType('nt:folder')) {
+            throw new \LogicException($path.' is not a folder but '.$node->getPrimaryNodeType()->getName());
+        }
+
+        return $node;
+    }
+
+    /**
+     * @param string $path   path relative to the $root.
+     * @param bool   $create whether to create the file if it does not exist yet.
+     *
+     * @return NodeInterface The node at $root/$path
+     */
+    private function getFileNode($path, $create = false)
+    {
+        $fileName = PathHelper::getNodeName('/'.$path);
+        $folderPath = PathHelper::getParentPath('/'.$path);
+        $folder = $this->getFolderNode($folderPath, $create);
+
+        if ($folder->hasNode($fileName) || !$create) {
+            return $folder->getNode($fileName);
+        }
+
+        $file = $folder->addNode($fileName, 'nt:file');
+        $file->addNode('jcr:content', 'nt:resource');
+
+        return $file;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($path)
+    {
+        $location = $this->applyPathPrefix($path);
+
+        return $this->session->nodeExists($location);
+    }
+
+    /**
+     * @param NodeInterface   $file
+     * @param string          $path path relative to the $root.
+     * @param string|resource $contents
+     * @param Config          $config
+     *
+     * @return array Metadata for return value.
+     */
+    private function writeMeta(NodeInterface $file, $path, $contents, Config $config)
+    {
+        $content = $file->getNode('jcr:content');
+        if (!$mimetype = $config->get('mimetype')) {
+            if (is_string($contents)) {
+                $mimetype = Util::guessMimeType($path, $contents);
+            } else {
+                $mimetype = null;
+            }
+        }
+        $content->setProperty('jcr:mimeType', $mimetype);
+        if ($encoding = $config->get('encoding')) {
+            $content->setProperty('jcr:encoding', $encoding);
+        }
+        $type = 'file';
+        $result = compact('mimetype', 'type', 'path');
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($path, $contents, Config $config)
+    {
+        $file = $this->getFileNode($path, true);
+        $content = $file->getNode('jcr:content');
+        $content->setProperty('jcr:data', $contents);
+
+        $result = $this->writeMeta($file, $path, $contents, $config);
+        $result['size'] = $content->getProperty('jcr:data')->getLength();
+        $result['contents'] = $contents;
+
+        $this->session->save();
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        $file = $this->getFileNode($path, true);
+        $content = $file->getNode('jcr:content');
+
+        $content->setProperty('jcr:data', $resource);
+
+        $this->session->save();
+
+        return $this->writeMeta($file, $path, $resource, $config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function readStream($path)
+    {
+        try {
+            $file = $this->getFileNode($path);
+        } catch (PathNotFoundException $e) {
+            return false;
+        }
+        $content = $file->getNode('jcr:content');
+        $result = $this->getFileInfo($file);
+        $result['stream'] = $content->getPropertyValue('jcr:data', PropertyType::BINARY);
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateStream($path, $resource, Config $config)
+    {
+        return $this->writeStream($path, $resource, $config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update($path, $contents, Config $config)
+    {
+        return $this->write($path, $contents, $config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($path)
+    {
+        try {
+            $file = $this->getFileNode($path);
+        } catch (PathNotFoundException $e) {
+            return false;
+        }
+        $content = $file->getNode('jcr:content');
+        $result = $this->getFileInfo($file);
+        $result['contents'] = $content->getPropertyValue('jcr:data', PropertyType::STRING);
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rename($path, $newpath)
+    {
+        $location = $this->applyPathPrefix($path);
+        if (!$this->session->nodeExists($location)) {
+            return false;
+        }
+        $destination = $this->applyPathPrefix($newpath);
+        $parentFolder = $this->applyPathPrefix(PathHelper::getParentPath($newpath));
+        $this->getFolderNode($parentFolder, true);
+
+        $this->session->move($location, $destination);
+        $this->session->save();
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function copy($path, $newpath)
+    {
+        $location = $this->applyPathPrefix($path);
+        if (!$this->session->nodeExists($location)) {
+            return false;
+        }
+        $destination = $this->applyPathPrefix($newpath);
+        $parentFolder = $this->applyPathPrefix(PathHelper::getParentPath($newpath));
+        $this->getFolderNode($parentFolder, true);
+
+        $this->session->save();
+        $this->session->getWorkspace()->copy($location, $destination);
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($path)
+    {
+        try {
+            $node = $this->getFileNode($path);
+        } catch (PathNotFoundException $e) {
+            return false;
+        }
+        $node->remove();
+        $this->session->save();
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        try {
+            $folder = $this->getFolderNode($directory);
+        } catch (PathNotFoundException $e) {
+            return array();
+        }
+
+        $result = [];
+        $files = [];
+        foreach ($folder->getNodes() as $node) {
+            $files[] = $node;
+        }
+        while ($file = array_shift($files)) {
+            $result[] = $this->getFileInfo($file, $this->removePathPrefix($file->getPath()));
+            if ($recursive && $file->isNodeType('nt:folder')) {
+                foreach ($file->getNodes() as $node) {
+                    $files[] = $node;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMetadata($path)
+    {
+        try {
+            $file = $this->getFileNode($path);
+        } catch (PathNotFoundException $e) {
+            return false;
+        }
+
+        return $this->getFileInfo($file);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMimetype($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTimestamp($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    private function getFileInfo(NodeInterface $file)
+    {
+        $type = $file->isNodeType('nt:folder') ? 'folder' : 'file';
+
+        $result = array(
+            'type' => $type,
+            'path' => $this->removePathPrefix($file->getPath()),
+        );
+
+        if ('file' === $type) {
+            $content = $file->getNode('jcr:content');
+            $result['size'] = $content->getProperty('jcr:data')->getLength();
+            $result['timestamp'] = $content->getPropertyValue('jcr:lastModified', PropertyType::LONG);
+            if ($content->hasProperty('jcr:mimeType')) {
+                $result['mimetype'] = $content->getPropertyValue('jcr:mimeType');
+            }
+            if ($content->hasProperty('jcr:encoding')) {
+                $result['encoding'] = $content->getPropertyValue('jcr:encoding');
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVisibility($path)
+    {
+        return array(
+            'visibility' => AdapterInterface::VISIBILITY_PUBLIC,
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setVisibility($path, $visibility)
+    {
+        return array(
+            'visibility' => AdapterInterface::VISIBILITY_PUBLIC,
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createDir($dirname, Config $config)
+    {
+        $this->getFolderNode($dirname, true);
+
+        return ['path' => $dirname, 'type' => 'dir'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteDir($dirname)
+    {
+        try {
+            $this->getFolderNode($dirname)->remove();
+        } catch (PathNotFoundException $e) {
+            return false;
+        } catch (\LogicException $e) {
+            return false;
+        }
+        $this->session->save();
+
+        return true;
+    }
+}
---
+++ tests/PhpcrAdapterTest.php
@@ -0,0 +1,316 @@
+<?php
+
+namespace League\Flysystem\Phpcr\Tests;
+
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\DriverManager;
+use Jackalope\RepositoryFactoryDoctrineDBAL;
+use League\Flysystem\Phpcr\PhpcrAdapter;
+use League\Flysystem\Config;
+use PHPCR\SessionInterface;
+use PHPCR\SimpleCredentials;
+
+class PhpcrAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Connection
+     */
+    private static $connection;
+
+    /**
+     * @var PhpcrAdapter
+     */
+    private $adapter;
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * @var string Path to the test root.
+     */
+    private $root;
+
+    public static function setUpBeforeClass()
+    {
+        static::$connection = DriverManager::getConnection(array(
+            'driver' => 'pdo_sqlite',
+            'path'   => sys_get_temp_dir().DIRECTORY_SEPARATOR.'flysystem-test.db',
+        ));
+        $options = array('disable_fks' => static::$connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform);
+        $repositorySchema = new \Jackalope\Transport\DoctrineDBAL\RepositorySchema($options, static::$connection);
+        $repositorySchema->reset();
+    }
+
+    public function setUp()
+    {
+        $this->root = '/flysystem_tests';
+
+        $factory = new RepositoryFactoryDoctrineDBAL();
+        $repository = $factory->getRepository(array(
+            'jackalope.doctrine_dbal_connection' => static::$connection,
+        ));
+        $this->session = $repository->login(new SimpleCredentials('test', 'test'));
+        if ($this->session->nodeExists($this->root)) {
+            $this->session->removeItem($this->root);
+        }
+        $this->adapter = new PhpcrAdapter($this->session, $this->root);
+    }
+
+    /**
+     * @expectedException \LogicException
+     */
+    public function testConstructor()
+    {
+        $this->session->getRootNode()->addNode('/flysystem_tests_broken', 'nt:unstructured');
+        new PhpcrAdapter($this->session, '/flysystem_tests_broken');
+    }
+
+    public function testHasWithDir()
+    {
+        $this->adapter->createDir('0', new Config());
+        $this->assertTrue($this->adapter->has('0'));
+        $this->adapter->deleteDir('0');
+    }
+
+    public function testHasWithFile()
+    {
+        $this->adapter->write('file.txt', 'content', new Config());
+        $this->assertTrue($this->adapter->has('file.txt'));
+        $this->adapter->delete('file.txt');
+    }
+
+    public function testReadNotFound()
+    {
+        $this->assertFalse($this->adapter->read('file.txt'));
+    }
+
+    public function testReadFolderNotFound()
+    {
+        $this->assertFalse($this->adapter->read('folder/file.txt'));
+    }
+
+    public function testReadStream()
+    {
+        $this->adapter->write('file.txt', 'contents', new Config());
+        $result = $this->adapter->readStream('file.txt');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('stream', $result);
+        $this->assertInternalType('resource', $result['stream']);
+        fclose($result['stream']);
+        $this->adapter->delete('file.txt');
+    }
+
+    public function testReadStreamNotFound()
+    {
+        $this->assertFalse($this->adapter->readStream('file.txt'));
+    }
+
+    public function testUpdate()
+    {
+        $this->adapter->update('file.txt', 'content', new Config());
+        $this->assertTrue($this->adapter->has('file.txt'));
+        $this->adapter->delete('file.txt');
+    }
+
+    public function testWriteStream()
+    {
+        $temp = tmpfile();
+        fwrite($temp, 'dummy');
+        rewind($temp);
+        $this->adapter->writeStream('dir/file.txt', $temp, new Config(['visibility' => 'public']));
+        //fclose($temp);
+        $this->assertTrue($this->adapter->has('dir/file.txt'));
+        $result = $this->adapter->read('dir/file.txt');
+        $this->assertEquals('dummy', $result['contents']);
+        $this->adapter->deleteDir('dir');
+    }
+
+    public function testUpdateStream()
+    {
+        $this->adapter->write('file.txt', 'initial', new Config());
+        $temp = tmpfile();
+        fwrite($temp, 'dummy');
+        $this->adapter->updateStream('file.txt', $temp, new Config());
+//        fclose($temp);
+        $this->assertTrue($this->adapter->has('file.txt'));
+        $this->adapter->delete('file.txt');
+    }
+
+    public function testCreateZeroDir()
+    {
+        $this->adapter->createDir('0', new Config());
+        $this->assertTrue($this->session->nodeExists($this->root.'/0'));
+        $this->adapter->deleteDir('0');
+    }
+
+    public function testRename()
+    {
+        $this->adapter->write('file.ext', 'content', new Config(['visibility' => 'public']));
+        $this->assertTrue($this->adapter->rename('file.ext', 'new.ext'));
+        $this->assertTrue($this->adapter->has('new.ext'));
+        $this->assertFalse($this->adapter->has('file.ext'));
+        $this->adapter->delete('file.ext');
+        $this->adapter->delete('new.ext');
+    }
+
+    public function testRenameNotExists()
+    {
+        $this->assertFalse($this->adapter->rename('file.ext', 'new.ext'));
+    }
+
+    public function testCopy()
+    {
+        $this->adapter->write('file.ext', 'content', new Config(['visibility' => 'public']));
+        $this->assertTrue($this->adapter->copy('file.ext', 'new.ext'));
+        $this->assertTrue($this->adapter->has('new.ext'));
+        $this->assertTrue($this->adapter->has('file.ext'));
+        $this->adapter->delete('file.ext');
+        $this->adapter->delete('new.ext');
+    }
+
+    public function testCopyNotExists()
+    {
+        $this->assertFalse($this->adapter->copy('file.ext', 'new.ext'));
+    }
+
+    public function testListContents()
+    {
+        $this->adapter->write('dirname/file.txt', 'contents', new Config());
+        $this->adapter->write('dirname/subfolder/file.txt', 'contents', new Config());
+        $contents = $this->adapter->listContents('dirname', false);
+        $this->assertCount(2, $contents);
+        $this->assertArrayHasKey('type', $contents[0]);
+        $this->assertEquals('file', $contents[0]['type']);
+        $this->assertArrayHasKey('type', $contents[1]);
+        $this->assertEquals('folder', $contents[1]['type']);
+    }
+
+    public function testListContentsRecursive()
+    {
+        $this->adapter->write('dirname/file.txt', 'contents', new Config());
+        $this->adapter->write('dirname/subfolder/file.txt', 'contents', new Config());
+        $contents = $this->adapter->listContents('dirname', true);
+        $this->assertCount(3, $contents);
+        $this->assertArrayHasKey('type', $contents[0]);
+        $this->assertEquals('file', $contents[0]['type']);
+        $this->assertArrayHasKey('type', $contents[1]);
+        $this->assertEquals('folder', $contents[1]['type']);
+        $this->assertArrayHasKey('type', $contents[2]);
+        $this->assertEquals('file', $contents[2]['type']);
+        $this->assertEquals('dirname/subfolder/file.txt', $contents[2]['path']);
+    }
+
+    public function testListingNonexistingDirectory()
+    {
+        $result = $this->adapter->listContents('nonexisting/directory');
+        $this->assertEquals([], $result);
+    }
+
+    public function testGetMetadataFolder()
+    {
+        $this->adapter->createDir('test', new Config());
+        $result = $this->adapter->getMetadata('test');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertEquals('folder', $result['type']);
+    }
+
+    public function testGetMetadataNotExisting()
+    {
+        $this->assertFalse($this->adapter->getMetadata('dummy.txt'));
+    }
+
+    public function testGetSize()
+    {
+        $this->adapter->write('dummy.txt', '1234', new Config());
+        $result = $this->adapter->getSize('dummy.txt');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('size', $result);
+        $this->assertEquals(4, $result['size']);
+    }
+
+    public function testGetTimestamp()
+    {
+        $this->adapter->write('dummy.txt', '1234', new Config());
+        $result = $this->adapter->getTimestamp('dummy.txt');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('timestamp', $result);
+        $this->assertInternalType('int', $result['timestamp']);
+    }
+
+    public function testGetMimetype()
+    {
+        $this->adapter->write('text.txt', 'contents', new Config());
+        $result = $this->adapter->getMimetype('text.txt');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('mimetype', $result);
+        $this->assertEquals('text/plain', $result['mimetype']);
+    }
+
+    public function testGetEncoding()
+    {
+        $this->adapter->write('text.txt', 'contents', new Config(array('encoding' => 'utf-8')));
+        $result = $this->adapter->getMimetype('text.txt');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('encoding', $result);
+        $this->assertEquals('utf-8', $result['encoding']);
+    }
+
+    public function testDeleteDir()
+    {
+        $this->adapter->write('nested/dir/path.txt', 'contents', new Config());
+        $this->assertTrue($this->session->nodeExists($this->root.'/nested/dir/path.txt'));
+        $this->assertTrue($this->adapter->deleteDir('nested'));
+        $this->assertFalse($this->adapter->has('nested/dir/path.txt'));
+        $this->assertFalse($this->session->nodeExists($this->root.'/nested'));
+    }
+
+    public function testDeleteDirNotFound()
+    {
+        $this->assertFalse($this->adapter->deleteDir('nested'));
+    }
+
+    public function testDeleteDirIsFile()
+    {
+        $this->adapter->write('dir/path.txt', 'contents', new Config());
+        $this->assertTrue($this->session->nodeExists($this->root.'/dir/path.txt'));
+        $this->assertFalse($this->adapter->deleteDir('dir/path.txt'));
+    }
+
+    public function testVisibilityPublic()
+    {
+        $this->adapter->write('path.txt', 'contents', new Config());
+        $this->adapter->setVisibility('path.txt', 'public');
+        $output = $this->adapter->getVisibility('path.txt');
+        $this->assertInternalType('array', $output);
+        $this->assertArrayHasKey('visibility', $output);
+        $this->assertEquals('public', $output['visibility']);
+    }
+
+    /**
+     * No visibility feature, always public
+     */
+    public function testVisibilityPrivate()
+    {
+        $this->adapter->write('path.txt', 'contents', new Config());
+        $this->adapter->setVisibility('path.txt', 'private');
+        $output = $this->adapter->getVisibility('path.txt');
+        $this->assertInternalType('array', $output);
+        $this->assertArrayHasKey('visibility', $output);
+        $this->assertEquals('public', $output['visibility']);
+    }
+
+    public function testApplyPathPrefix()
+    {
+        $this->adapter->setPathPrefix('');
+        $this->assertEquals('', $this->adapter->applyPathPrefix(''));
+    }
+
+    public function testApplyPathPrefixAbsolute()
+    {
+        $this->adapter->setPathPrefix($this->root . '/');
+        $this->assertEquals($this->root, $this->adapter->applyPathPrefix('/'));
+    }
+}
